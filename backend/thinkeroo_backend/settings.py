3af62506517b0d0a7ccc48248409cfdb90bd5d26
elfinix <axllouis.coronel@gmail.com>
2024-12-12 05:41:32 +0800

    Revert database configuration to MySQL in settings.py

--- backend/thinkeroo_backend/settings.py
+++ backend/thinkeroo_backend/settings.py
@@ -88,30 +88,23 @@ WSGI_APPLICATION = 'thinkeroo_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "thinkeroo",
-#         "USER": "root",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "3306",
-#         "OPTIONS": {
-#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-#             "charset": "utf8mb4",
-#             "ssl": {"ssl": {"ca": ""}},  # Disable SSL
-#         },
-
-#     }
-# }
-
-# switch to local database for now
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "thinkeroo",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+            "ssl": {"ssl": {"ca": ""}},  # Disable SSL
+        },
+
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
